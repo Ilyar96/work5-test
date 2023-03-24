453584cc7804beb8aca3947152e8d52c1db9b9d5
Ilyar <kasyymov.ilyar@gmail.com>
2023-03-24 14:34:51 +0300

    added header, content, modal, mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,20 @@
 // Import our custom CSS
-import '../scss/styles.scss'
+import "../scss/styles.scss";
+import { SelectConstructor } from "./select";
+import "./dynamicAdapt";
 
 // Import only the Bootstrap components we need
-import { Popover } from 'bootstrap';
+import { Modal } from "bootstrap";
 
-// Create an example popover
-document.querySelectorAll('[data-bs-toggle="popover"]')
-  .forEach(popover => {
-    new Popover(popover)
-  })
+new SelectConstructor({});
+
+const menuToggleHandler = (btnSelector, value) => {
+	const btn = document.querySelector(btnSelector);
+
+	btn.addEventListener("click", (e) => {
+		document.body.classList.toggle("menu-open");
+	});
+};
+
+menuToggleHandler(".menu-btn");
+menuToggleHandler(".menu-background");
